590dd0c80971526c61c016324cd1a84139c31353
M <superrmarcis@gmail.com>
2024-06-20 01:55:15 +0300

    kill me fix

--- database/migrations/2024_06_19_164123_create_payment_detail_table.php
+++ database/migrations/2024_06_19_164123_create_payment_detail_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentDetailTable extends Migration
+class CreatePaymentDetailsTable extends Migration
 {
     public function up()
     {
